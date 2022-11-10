51a83273cd3f68239644e3c5416d0515365bb2db
RossBurton <Ross.Burton@zifornd.com>
2022-11-10 17:44:30 +0000

    Fixed quotation marks in setup script

--- project_setup.sh
+++ project_setup.sh
@@ -3,7 +3,7 @@ PROJECT_NAME=$1
 hatch new $PROJECT_NAME
 cd $PROJECT_NAME
 curl https://raw.githubusercontent.com/zifods/config_templates/main/pyproject.toml > pyproject.toml
-sed -i “s/example/${PROJECT_NAME}/” pyproject.toml
+sed -i "s/example/${PROJECT_NAME}/" pyproject.toml
 hatch env create
 hatch shell
 git add .
